092e69942a23fff76eda58dd16a577f2d08e5001
Mak <lievrard.maxime@icloud.com>
2022-02-14 23:19:16 +0100

    sign in et sign up

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ const btnSale = document.querySelector(".btn-sale");
 const categoryLinks = document.querySelector(".category-links");
 const collectionLinks = document.querySelector(".collection-links");
 
-btnHome.addEventListener("click", function() {
+btnHome.addEventListener("click", function () {
     btnHome.classList = "col-11 m-1 border btn-home";
     btnCategories.classList = "col-10 m-1 border btn-categories";
     btnCollections.classList = "col-10 m-1 border btn-collections";
@@ -14,7 +14,7 @@ btnHome.addEventListener("click", function() {
     collectionLinks.style.display = "none";
 })
 
-btnCategories.addEventListener("click", function() {
+btnCategories.addEventListener("click", function () {
     btnCategories.classList = "col-11 m-1 border btn-categories";
     btnHome.classList = "col-10 m-1 border btn-home";
     btnCollections.classList = "col-10 m-1 border btn-collections";
@@ -28,7 +28,7 @@ btnCategories.addEventListener("click", function() {
     }
 })
 
-btnCollections.addEventListener("click", function() {
+btnCollections.addEventListener("click", function () {
     btnCollections.classList = "col-11 m-1 border btn-collections";
     btnHome.classList = "col-10 m-1 border btn-home";
     btnCategories.classList = "col-10 m-1 border btn-categories";
@@ -42,7 +42,7 @@ btnCollections.addEventListener("click", function() {
     }
 })
 
-btnSale.addEventListener("click", function() {
+btnSale.addEventListener("click", function () {
     btnSale.classList = "col-11 m-1 border btn-sale";
     btnHome.classList = "col-10 m-1 border btn-home";
     btnCategories.classList = "col-10 m-1 border btn-categories";
@@ -55,47 +55,68 @@ btnSale.addEventListener("click", function() {
 function moveToSelected(element) {
 
     if (element == "next") {
-      var selected = $(".selected").next();
+        var selected = $(".selected").next();
     } else if (element == "prev") {
-      var selected = $(".selected").prev();
+        var selected = $(".selected").prev();
     } else {
-      var selected = element;
+        var selected = element;
     }
-  
+
     var next = $(selected).next();
     var prev = $(selected).prev();
     var prevSecond = $(prev).prev();
     var nextSecond = $(next).next();
-  
+
     $(selected).removeClass().addClass("selected");
-  
+
     $(prev).removeClass().addClass("prev");
     $(next).removeClass().addClass("next");
-  
+
     $(nextSecond).removeClass().addClass("nextRightSecond");
     $(prevSecond).removeClass().addClass("prevLeftSecond");
-  
+
     $(nextSecond).nextAll().removeClass().addClass('hideRight');
     $(prevSecond).prevAll().removeClass().addClass('hideLeft');
-  
-  }
-  
-  // Eventos teclado
-  $(document).keydown(function(e) {
-      switch(e.which) {
-          case 37: // left
-          moveToSelected('prev');
-          break;
-  
-          case 39: // right
-          moveToSelected('next');
-          break;
-  
-          default: return;
-      }
-      e.preventDefault();
-  });
-  
-  $('#carousel div').click(function() {
+
+}
+
+// Eventos teclado
+$(document).keydown(function (e) {
+    switch (e.which) {
+        case 37: // left
+            moveToSelected('prev');
+            break;
+
+        case 39: // right
+            moveToSelected('next');
+            break;
+
+        default:
+            return;
+    }
+    e.preventDefault();
+});
+
+$('#carousel div').click(function () {
     moveToSelected($(this));
-  });
+});
+
+// Inscription - Connexion
+const signIn = document.querySelector(".sign-in");
+const signUp = document.querySelector(".sign-up");
+const connexion = document.querySelector(".connexion");
+const inscription = document.querySelector(".inscription");
+
+signIn.addEventListener("click", function () {
+    signUp.style.border = "none";
+    signIn.style.borderBottom = "1px solid black";
+    inscription.style.display = "none";
+    connexion.style.display = "block";
+});
+
+signUp.addEventListener("click", function () {
+    signIn.style.border = "none";
+    signUp.style.borderBottom = "1px solid black";
+    inscription.style.display = "block";
+    connexion.style.display = "none";
+});
